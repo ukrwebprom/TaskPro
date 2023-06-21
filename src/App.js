b30392d1d23634cc1262de1b60b2f8677770c6f4
Volodymyr <mospan.volodymyr@gmail.com>
2023-06-21 23:57:30 +0300

    fix BoardForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { BoardForm } from "components/forms/BoardForm/BoardForm";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>TaskPro</h1>
-      <BoardForm />
     </div>
   );
 }

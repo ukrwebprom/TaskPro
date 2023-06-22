d02eec2788550d1255483b54e7d5bf5f256bd858
ukrwebprom <ukrwebprom@gmail.com>
2023-06-22 21:29:25 +0300

    main layout for home page

---
+++ src/components/HomeLayout/HomeLayout.jsx
@@ -0,0 +1,21 @@
+import css from './HomeLayout.module.css';
+import { useState } from 'react';
+export const HomeLayout = ({children}) => {
+    const [isSidebar, setIsSidebar] = useState(false);
+
+    return(
+        <div className={css.outer}>
+            <div className={isSidebar? css.sidebar : css.hidden}>
+                
+                Sidebar
+            </div>
+            {isSidebar && <div className={css.backdrop} onClick={() => setIsSidebar(false)}></div>}
+            <div className={css.mainarea}>
+                <div className={css.header}>
+                    <button onClick={() => setIsSidebar(true)}>Show sidebar</button>
+                </div>
+                {children}
+            </div>
+        </div>
+    )
+}

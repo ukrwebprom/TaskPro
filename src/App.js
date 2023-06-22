e0c92e00ccac6d07778ef960c4b06b4940b2cb7e
ukrwebprom <ukrwebprom@gmail.com>
2023-06-22 08:57:35 +0300

    theme variables implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import './App.css';
 import {useTheme} from './hooks/useTheme';
 import { useUser } from './hooks/useUser';
 import { ThemeSelector } from './components/ThemeSelector/ThemeSelector';
-import { useEffect } from 'react';
 
 function App() {
   const { theme } = useTheme();
   const {isLogged} = useUser();
-  console.log(isLogged)
 
   return (
     <div className="App" data-theme={theme}>

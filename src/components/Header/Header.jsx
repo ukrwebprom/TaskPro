87d143875b310e58e39fea8ab7f270b574afc8ac
SedaNoty <sedanoty5@gmail.com>
2023-06-24 17:19:57 +0300

    plug toggle

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,32 @@
 import css from './Header.module.css'
 import { ThemeSelector } from 'components/ThemeSelector/ThemeSelector'
 
-export const Header = ({ onClick }) => {
+export const Header = () => {
   return (
-    <div className={css.headerTaskPro}>
-      <div className={css.outputselector}>
-        <button onClick={onClick} className={css.styleTheme} > <span className={css.spantheme}>Theme</span>
-          <img className={css.icontheme} src="/TaskPro/static/media/icon.811b950c1109ac38463be92ae94fe634.svg" alt='staticlogo'></img>
-          {/* <svg className={css.icontheme}><use href="./../../../public/logo192.png"></use> </svg>  */}
-        </button>
-        <div className={css.selectortheme}>
-          <ThemeSelector />
-        </div>
+    <div className={css.headerall}>
+      <div className={css.menuburger}>
+          <button className={css.burgerstyle}></button>
       </div>
 
-      <ul className={css.styleUserInfo}>
-          <li className={css.styleName}>Name</li>
-          <li className={css.styleAvatar}>
-              <button className={css.styleAvatar}></button>
-          </li>
-      </ul>
+      <div className={css.headerTaskPro}>
+        <div className={css.outputselector}>
+          <button className={css.styleTheme} > <span className={css.spantheme}>Theme</span>
+            <img className={css.icontheme} src="/TaskPro/static/media/icon.811b950c1109ac38463be92ae94fe634.svg" alt='staticlogo'></img>
+            {/* <svg className={css.icontheme}><use href="./../../../public/logo192.png"></use> </svg>  */}
+          </button>
+          <div className={css.selectortheme}>
+            <ThemeSelector />
+          </div>
+        </div>
+
+        <ul className={css.styleUserInfo}>
+            <li className={css.styleName}>Name</li>
+            <li className={css.styleAvatar}>
+                <button className={css.styleAvatar}></button>
+            </li>
+        </ul>
+      </div>
     </div>
+    
   )
 }

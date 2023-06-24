3eca7718259fd61aaa2180334e28446f54ac6379
SedaNoty <sedanoty5@gmail.com>
2023-06-24 13:21:03 +0300

    header edit 3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,14 @@ import { ThemeSelector } from 'components/ThemeSelector/ThemeSelector'
 export const Header = () => {
   return (
     <div className={css.headerTaskPro}>
-      <ThemeSelector />
-      <div>
+      <div className={css.outputselector}>
         <a href="/" className={css.styleTheme} > Theme
           <img src="/TaskPro/static/media/icon.811b950c1109ac38463be92ae94fe634.svg" alt='staticlogo'></img>
           {/* <svg className={css.icontheme}><use href="./../../../public/logo192.png"></use> </svg>  */}
         </a>
+        <div className={css.selectortheme}>
+          <ThemeSelector />
+        </div>
       </div>
 
       <ul className={css.styleUserInfo}>

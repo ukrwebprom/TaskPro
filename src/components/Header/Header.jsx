8fbc9e43a1e0d1650d343196849f744bf40611cc
SedaNoty <sedanoty5@gmail.com>
2023-06-24 15:08:25 +0300

    background img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
 import css from './Header.module.css'
 import { ThemeSelector } from 'components/ThemeSelector/ThemeSelector'
 
-export const Header = () => {
+export const Header = ({ onClick }) => {
   return (
     <div className={css.headerTaskPro}>
       <div className={css.outputselector}>
-        <a href="/" className={css.styleTheme} > Theme
-          <img src="/TaskPro/static/media/icon.811b950c1109ac38463be92ae94fe634.svg" alt='staticlogo'></img>
+        <button onClick={onClick} className={css.styleTheme} > <span className={css.spantheme}>Theme</span>
+          <img className={css.icontheme} src="/TaskPro/static/media/icon.811b950c1109ac38463be92ae94fe634.svg" alt='staticlogo'></img>
           {/* <svg className={css.icontheme}><use href="./../../../public/logo192.png"></use> </svg>  */}
-        </a>
+        </button>
         <div className={css.selectortheme}>
           <ThemeSelector />
         </div>

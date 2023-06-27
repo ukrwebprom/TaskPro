b5f88dbb21b5c6121d5bd1ed5746a1e113ebe6eb
TetianaTantrum <tetiana.tantrum@gmail.com>
2023-06-27 17:16:11 +0300

    ok

--- src/components/HomeLayout/HomeLayout.jsx
+++ src/components/HomeLayout/HomeLayout.jsx
@@ -1,29 +1,29 @@
-import css from './HomeLayout.module.css';
-import { useState } from 'react';
-import Sidebar from 'components/Sidebar/Sidebar';
-import { useUser } from 'hooks/useUser';
-import { Header } from 'components/Header/Header';
-import Filters from 'components/Filters/Filters';
-import Task from 'components/Task/Task';
+import css from "./HomeLayout.module.css";
+import { useState } from "react";
+import Sidebar from "components/Sidebar/Sidebar";
+import { useUser } from "hooks/useUser";
+import { Header } from "components/Header/Header";
+import Task from "components/Task/Task";
 
-export const HomeLayout = ({children}) => {
-    const [isSidebar, setIsSidebar] = useState(false);
-    const { theme } = useUser();
+export const HomeLayout = ({ children }) => {
+  const [isSidebar, setIsSidebar] = useState(false);
+  const { theme } = useUser();
 
-    return(
-        <div className={css.outer} data-theme={theme}>
-            <div className={isSidebar? css.sidebar : css.hidden}>
-                <Sidebar />
-            </div>
-            {isSidebar && <div className={css.backdrop} onClick={() => setIsSidebar(false)}></div>}
-            <div className={css.mainarea}>
-            
-                <div className={css.header}>
-                    <Header toggleSidebar={setIsSidebar}/>
-                </div>
-                
-                {/* TODO: If you dont want see single task comment next div. */}
-{/*                 <div style={{ paddingLeft: 300 }}>
+  return (
+    <div className={css.outer} data-theme={theme}>
+      <div className={isSidebar ? css.sidebar : css.hidden}>
+        <Sidebar />
+      </div>
+      {isSidebar && (
+        <div className={css.backdrop} onClick={() => setIsSidebar(false)}></div>
+      )}
+      <div className={css.mainarea}>
+        <div className={css.header}>
+          <Header toggleSidebar={setIsSidebar} />
+        </div>
+
+        {/* TODO: If you dont want see single task comment next div. */}
+        {/*                 <div style={{ paddingLeft: 300 }}>
                   <Task
                     columnList={[{name: 'In progress'}, {name: 'Done'}]}
                     taskData={{
@@ -36,9 +36,8 @@ export const HomeLayout = ({children}) => {
                     }}
                   />
                 </div> */}
-                {children}
-            </div>
-            
-        </div>
-    )
-}
+        {children}
+      </div>
+    </div>
+  );
+};

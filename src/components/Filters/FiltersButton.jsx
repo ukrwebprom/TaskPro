7b8c16f5f00733b628af8f0cb22a8e242cb90e90
TetianaTantrum <tetiana.tantrum@gmail.com>
2023-06-28 11:17:38 +0300

    FilterNew

--- src/components/Filters/FiltersButton.jsx
+++ src/components/Filters/FiltersButton.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import css from "./Filters.module.css";
+import Icon from "components/Icon/Icon";
 
 const FiltersButton = ({ onClick }) => {
   return (
     <button className={css.filtersButton} onClick={onClick}>
+      <Icon
+        className={css.filterIcon}
+        name={"#filter-icon"}
+        sprite={2}
+        width="16"
+        height="16"
+      />
       Filters
     </button>
   );

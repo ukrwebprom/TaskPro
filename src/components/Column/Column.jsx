a2503d6666d60299737949bbcc58e79fb11d238e
RostyslavKr <106476898+RostyslavKr@users.noreply.github.com>
2023-07-02 12:17:33 +0300

    add hover,focus to button and Icon edit and delete

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,23 +1,24 @@
-import { useMemo } from "react";
+import { useMemo } from 'react';
 import { nanoid } from 'nanoid';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ColumnForm } from 'components/forms/ColumnForm/ColumnForm';
 import { CardForm } from 'components/forms/CardForm/CardForm';
-import { updateColumnTitle, deleteColumn, addTask } from 'redux/boards/operations';
+import {
+  updateColumnTitle,
+  deleteColumn,
+  addTask,
+} from 'redux/boards/operations';
 import Modal from '../Modal/Modal';
 import Task from 'components/Task/Task';
 import Icon from '../Icon';
-import Button from "components/Button/Button";
+import Button from 'components/Button/Button';
 import css from './Column.module.css';
-import { useModal } from "hooks/useModal";
-import { setFilter } from "redux/boards/selectors";
+import { useModal } from 'hooks/useModal';
+import { setFilter } from 'redux/boards/selectors';
 
-export const Column = ({
-  allColumns,
-  data,
-}) => {
-  const {getModal, killModal} = useModal();
+export const Column = ({ allColumns, data }) => {
+  const { getModal, killModal } = useModal();
   const dispatch = useDispatch();
   const filter = useSelector(setFilter);
   const [showColumnModal, setShowColumnModal] = useState(false);
@@ -25,63 +26,75 @@ export const Column = ({
 
   const handleEditColumn = value => {
     killModal();
-    dispatch(updateColumnTitle({...data, ...value}));
-  }
+    dispatch(updateColumnTitle({ ...data, ...value }));
+  };
   const handleAddTask = value => {
-    dispatch(addTask({_id:data._id, body:value}))
+    dispatch(addTask({ _id: data._id, body: value }));
     killModal();
-  }
+  };
   const handleDelete = () => dispatch(deleteColumn(data._id));
-  
+
   const avaliableColumns = useMemo(() => {
-    const newColumns = {...allColumns};
+    const newColumns = { ...allColumns };
     delete newColumns[data._id];
     return newColumns;
   }, [allColumns, data]);
 
   return (
     <>
-    <section className={css.containerColumn}>
-      <div className={css.wrapperTitleColumn}>
-        <h3 className={css.titleColumn}>{data.title}</h3>
-        <div className={css.wrapperButton}>
-          <button
-            className={css.buttonColumn}
-            type="button"
-            onClick={() => getModal("Edit column", 
-            <ColumnForm defaultValues={{title:data.title}} setTitle={handleEditColumn} />
-            )}>
-            <Icon name={'#pencil-icon'} />
+      <section className={css.containerColumn}>
+        <div className={css.wrapperTitleColumn}>
+          <h3 className={css.titleColumn}>{data.title}</h3>
+          <div className={css.wrapperButton}>
+            <button
+              className={`${css.buttonColumn} ${css.editIcon}`}
+              type="button"
+              onClick={() =>
+                getModal(
+                  'Edit column',
+                  <ColumnForm
+                    defaultValues={{ title: data.title }}
+                    setTitle={handleEditColumn}
+                  />
+                )
+              }
+            >
+              <Icon name={'#pencil-icon'} />
             </button>
-            <button className={css.buttonColumn} onClick={handleDelete}>
-            <Icon name={'#trash-icon'} />
+            <button
+              className={`${css.buttonColumn} ${css.deleteIcon}`}
+              onClick={handleDelete}
+            >
+              <Icon name={'#trash-icon'} />
             </button>
           </div>
         </div>
 
-      <div className={css.columnMiddle}>
-      <ul className={css.listTask}>
-        {data.tasks &&
-          data.tasks
-          .filter((task) => !filter || task.priority === filter)
-          .map((task, idx) => 
-            (
-              <Task
-                avaliableColumns={avaliableColumns}
-                index={idx}
-                key={task._id}
-                taskData={task}
-                colId={data._id}
-                columnList={[{name: 'todo'}, {name: 'Done'}]}
-              />
-            )
-          )}
-      </ul>
-      </div>
-      <Button title="Add another card" type="button" 
-      action={() => getModal("Add card", <CardForm setTask={handleAddTask} />
-      )}/>
-    </section>
+        <div className={css.columnMiddle}>
+          <ul className={css.listTask}>
+            {data.tasks &&
+              data.tasks
+                .filter(task => !filter || task.priority === filter)
+                .map((task, idx) => (
+                  <Task
+                    avaliableColumns={avaliableColumns}
+                    index={idx}
+                    key={task._id}
+                    taskData={task}
+                    colId={data._id}
+                    columnList={[{ name: 'todo' }, { name: 'Done' }]}
+                  />
+                ))}
+          </ul>
+        </div>
+        <Button
+          title="Add another card"
+          type="button"
+          action={() =>
+            getModal('Add card', <CardForm setTask={handleAddTask} />)
+          }
+        />
+      </section>
     </>
   );
 };
--- src/components/Sidebar/Boards/BoardsItem/BoardsItem.jsx
+++ src/components/Sidebar/Boards/BoardsItem/BoardsItem.jsx
@@ -1,25 +1,27 @@
-import Icon from "components/Icon/Icon";
-import css from "../../Sidebar.module.css";
+import Icon from 'components/Icon/Icon';
+import css from '../../Sidebar.module.css';
 
 const BoardsItem = ({ index, board, setActive, handleDelete, handleEdit }) => {
   return (
     <>
-      <div
-        className={css.boardLink}
-        onClick={() => setActive(index)}>
+      <div className={css.boardLink} onClick={() => setActive(index)}>
         <div className={css.boardInfo}>
-          <Icon name={"#" + board.icon} width="18" height="18" />
+          <Icon name={'#' + board.icon} width="18" height="18" />
           <span className={css.boardTitle}>{board.title}</span>
         </div>
         <div className={css.boardButtons}>
-          <button type="button" className={css.boardActionButton}
-          onClick={handleEdit}>
+          <button
+            type="button"
+            className={`${css.boardActionButton} ${css.editIcon}`}
+            onClick={handleEdit}
+          >
             <Icon name="#pencil-icon" />
           </button>
           <button
             type="button"
-            className={css.boardActionButton}
-            onClick={() => handleDelete(board._id)}>
+            className={`${css.boardActionButton} ${css.deleteIcon}`}
+            onClick={() => handleDelete(board._id)}
+          >
             <Icon name="#trash-icon" />
           </button>
         </div>

480d2b3ee824eed5c17d57b85490358fd088094d
ukrwebprom <ukrwebprom@gmail.com>
2023-07-06 02:20:45 +0300

    welcome upd

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,7 +2,6 @@ import React from "react";
 import { WelcomeLayout } from "components/WelcomeLayout/WelcomeLayout";
 import s from "..//components/WelcomeLayout/WelcomeLayout.module.css";
 import { Suspense } from "react";
-import g from '../images/googlesignup.png'
 import mainimg from '../images/welcome-page.png'
 import Icon from 'components/Icon/Icon'
 
@@ -24,11 +23,8 @@ const Welcome = () => {
         </p>
         <a className={s.regbtn} href="auth/register">Registration</a>
         <a className={s.logintext} href="auth/login">Log In</a>
-        {/* <div  className={s.gogglelogin}>
-        <img  className={s.goggleimage} src={g} alt="Google icon" /> */}
         <a  className={s.gogglelogin} href={"https://taskpro-41yf.onrender.com/user/google"}>
           Sign in with Google</a>
-{/*         </div> */}
       </WelcomeLayout>
     </Suspense>
   );

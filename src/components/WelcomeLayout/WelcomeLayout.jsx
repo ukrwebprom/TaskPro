61de2d3fc3a25ebe95c63a037f1dabd3cccdb824
ukrwebprom <ukrwebprom@gmail.com>
2023-07-06 02:14:56 +0300

    google

--- src/components/WelcomeLayout/WelcomeLayout.jsx
+++ src/components/WelcomeLayout/WelcomeLayout.jsx
@@ -1,22 +1,10 @@
 import css from './WelcomeLayout.module.css'
-import mainimg from '../../images/welcome-page.png'
-import Icon from 'components/Icon/Icon'
+
 export const WelcomeLayout = ({children}) => {
     return(
         <div className={css.container}>
             <div className={css.content}>
-            <img className={css.wecomeimage}
-            src= {mainimg} 
-            alt="welcome to TaskPro" />
 
-            <div className={css.logowrap}>
-            <Icon name="#icon-welcome-icon" width="48" height="48" />
-            <h1 className={css.title}>Task Pro</h1>
-            </div>
-            <p className={css.paragraph}>
-            Supercharge your productivity and take control of your tasks with
-            Task Pro - Don't wait, start achieving your goals now!
-            </p>
                 {children}
             </div>
         </div>
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -3,17 +3,32 @@ import { WelcomeLayout } from "components/WelcomeLayout/WelcomeLayout";
 import s from "..//components/WelcomeLayout/WelcomeLayout.module.css";
 import { Suspense } from "react";
 import g from '../images/googlesignup.png'
+import mainimg from '../images/welcome-page.png'
+import Icon from 'components/Icon/Icon'
+
 const Welcome = () => {
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <WelcomeLayout>
+        <img className={s.wecomeimage}
+            src= {mainimg} 
+            alt="welcome to TaskPro" />
+
+        <div className={s.logowrap}>
+            <Icon name="#icon-welcome-icon" width="48" height="48" />
+            <h1 className={s.title}>Task Pro</h1>
+        </div>
+        <p className={s.paragraph}>
+            Supercharge your productivity and take control of your tasks with
+            Task Pro - Don't wait, start achieving your goals now!
+        </p>
         <a className={s.regbtn} href="auth/register">Registration</a>
         <a className={s.logintext} href="auth/login">Log In</a>
-        <div  className={s.gogglelogin}>
-        <img  className={s.goggleimage} src={g} alt="Google icon" />
-        <a  className={s.logintext} href={"https://taskpro-41yf.onrender.com/user/google"}>
+        {/* <div  className={s.gogglelogin}>
+        <img  className={s.goggleimage} src={g} alt="Google icon" /> */}
+        <a  className={s.gogglelogin} href={"https://taskpro-41yf.onrender.com/user/google"}>
           Sign in with Google</a>
-        </div>
+{/*         </div> */}
       </WelcomeLayout>
     </Suspense>
   );

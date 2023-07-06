e1270482f8ab0cb78b203fbe3369e2e2be99cece
ukrwebprom <ukrwebprom@gmail.com>
2023-07-06 17:09:51 +0300

    fix background

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -8,7 +8,7 @@ export const Background = ({children}) => {
     const isWide = useMedia('(min-width: 1280px)');
     const isMobile = useMedia('(max-width: 320px)');
     const userRatio = useMedia('(-webkit-min-device-pixel-ratio: 2)')? "2x":"1x";
-    let bg = {backgroundColor:'white'};
+    let bg = {};
     if(selectedBgIndex !== "0") {
         const userScreen = isWide? "1280" : isMobile? "320-min" : "768-min";
         const url = `https://res.cloudinary.com/pro-task-cloud/image/upload/v1687806931/backgrounds/diego-ph-wyeapf7Gy-U-unsplash%20${selectedBgIndex}%40${userRatio}_${userScreen}.jpg.webp`;

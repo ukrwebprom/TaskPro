e6970b5cc74e38570468ad00ea88c0a1f205cf19
ukrwebprom <ukrwebprom@gmail.com>
2023-07-06 17:39:24 +0300

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,14 @@
 import css from './Btn.module.css'
-import Icon from 'components/Icon/Icon';
+import sprite from '../../images/sprite.svg'
 
 const Button = ({ type = 'button', title, icon=true, invert=false, action }) => {
   return (
     <button type={type} className={invert? css.invertBtn : css.btn} onClick={action} >
       {icon && 
         <div className={invert? css.invertIcon : css.wrapperIcon}>
-          <Icon name={'#plus-icon'} />
+          <svg width='18px' height='18px' stroke="currentColor">
+                      <use href={sprite + "#plus-icon"} />
+          </svg>
         </div>}
       <span className={css.title}>{title}</span>
     </button>
